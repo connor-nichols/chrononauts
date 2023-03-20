3517e3aa6d5094a488f920c5842a61333e54c8b3
Benjamin Cook <90069170+bencook9@users.noreply.github.com>
2023-03-20 16:45:14 -0500

    Starting scripting for inventory

--- Assets/Scripts/InventorySystem.cs
+++ Assets/Scripts/InventorySystem.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InventorySystem : MonoBehaviour
+namespace Valve.VR.InteractionSystem.Sample
 {
-    // Start is called before the first frame update
-    void Start()
+    public class InventorySystem : MonoBehaviour
     {
-        
-    }
+        private Interactable interactable;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Update()
+        {
+            if (interactable.attachedToHand)
+            {
+                GameObject item = interactable.gameObject;
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            other.gameObject.transform.position = new Vector3(0, 0, 0);
+            other.gameObject.GetComponent<Rigidbody>().useGravity = false;
+            // when object collides with trigger, it will become a child of the trigger
+        }
     }
 }
