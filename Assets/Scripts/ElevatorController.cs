0410c7f48cb04a60fc5a9a56c135b446d4ab3b35
viet-ninh <78924733+viet-ninh@users.noreply.github.com>
2023-03-27 16:46:27 -0500

    Adding rotating door to elevator template

--- Assets/Scripts/ElevatorController.cs
+++ Assets/Scripts/ElevatorController.cs
@@ -3,18 +3,51 @@ using UnityEngine;
 public class ElevatorController : MonoBehaviour
 {
     private bool doorClosed = false;
+    private Quaternion startRotation;
+    private Quaternion endRotation;
+    private float timeElapsed = 0.0f;
+
+    public GameObject door;
+
+    public float duration = 1.0f;
 
     public void DoorOperator()
     {
         print("operation accessed");
-        if(doorClosed)
+        if (doorClosed)
+        {
+            rotateDoor();
             doorClosed = false;
+
+        }
         else
+        {
+            rotateDoor();
             doorClosed = true;
+
+        }
     }
 
     public bool getDoorPosition()
     {
         return doorClosed;
+    }
+
+    private void rotateDoor()
+    {
+        timeElapsed += Time.deltaTime;
+
+        // Calculate the interpolation factor
+        float t = Mathf.Clamp01(timeElapsed / duration);
+
+        if (doorClosed)
+        {
+            // Interpolate between the start and end rotation
+            transform.rotation = Quaternion.Lerp(Quaternion.Euler(0, -90, 0), Quaternion.Euler(0, 0, 0), t);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Lerp(Quaternion.Euler(0, 0, 0), Quaternion.Euler(0, -90, 0), t);
+        }
     }
 }
