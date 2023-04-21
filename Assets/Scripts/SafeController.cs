d3b592d37f14f6ddfabef6cb51683a1274a507fe
viet-ninh <78924733+viet-ninh@users.noreply.github.com>
2023-04-21 17:05:25 -0500

    Added sounds to play when the dial is rotated o a specific poin

--- Assets/Scripts/SafeController.cs
+++ Assets/Scripts/SafeController.cs
@@ -5,26 +5,33 @@ using Valve.VR.InteractionSystem;
 
 
 [RequireComponent(typeof(Interactable))]
+[RequireComponent(typeof(CircularDrive))]
 public class SafeController : MonoBehaviour
 {
-    private Interactable interactable;
-    private GameObject lockPlane;
+    private AudioSource audio;
+    private CircularDrive circularDrive;
+    private bool clicked;
+    private bool clicked2;
 
     // Start is called before the first frame update
     void Start()
     {
-        interactable = GetComponent<Interactable>();
-        lockPlane = GetComponent<GameObject>();
+        circularDrive = GetComponent<CircularDrive>();
+        audio = GetComponent<AudioSource>();
     }
 
-
-    public void playMinClick()
+    public void Update()
     {
-        print("min click");
-    }
+        if (circularDrive.outAngle > 100 && !clicked)
+        {
+            clicked = true;
+            audio.Play();
+        }
 
-    public void playClickNoise()
-    {
-        print("click");
+        if (circularDrive.outAngle < -100 && !clicked2 && clicked)
+        {
+            clicked2 = true;
+            audio.Play();
+        }
     }
 }
