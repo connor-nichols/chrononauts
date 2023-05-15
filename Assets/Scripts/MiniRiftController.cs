ac8baaadc0fa7674cd38bd23429a0411e243d01c
Benjamin Cook <90069170+bencook9@users.noreply.github.com>
2023-05-15 16:22:01 -0500

    Updated mini rift despawn time

--- Assets/Scripts/MiniRiftController.cs
+++ Assets/Scripts/MiniRiftController.cs
@@ -33,7 +33,7 @@ public class MiniRiftController : MonoBehaviour
         // Need to add audio/haptics when TutorialRift is being "destroyed"
         // Need to make it so TutorialRift doesn't get spawned once destoryed
         transform.Rotate(new Vector3(270, 270, 270) * Time.deltaTime);
-        yield return new WaitForSeconds(2.5f);
+        yield return new WaitForSeconds(5f);
         //Destroy(transform.gameObject);
         transform.gameObject.SetActive(false);
     }
