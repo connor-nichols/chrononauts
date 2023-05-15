1a47b7619ced674b3a90d9bd1509a504755fd39b
Benjamin Cook <90069170+bencook9@users.noreply.github.com>
2023-05-15 16:42:27 -0500

    Updated Roomtone sounds

--- Assets/Scripts/LevelTeleporter.cs
+++ Assets/Scripts/LevelTeleporter.cs
@@ -12,11 +12,14 @@ public class LevelTeleporter : MonoBehaviour
     public ElevatorController Elevator;
 
     public GameObject elevatorButton;
+
+    private GameObject playerObject;
     // private AudioSource audioSource;
 
     private void Start()
     {
         // audioSource = GetComponent<AudioSource>();
+        playerObject = GameObject.Find("Player");
     }
 
     public void OnButtonDown(Hand fromHand)
@@ -47,5 +50,29 @@ public class LevelTeleporter : MonoBehaviour
         SceneManager.LoadScene(SceneDestination);
         // maybe change this to the complete elevator
         AkSoundEngine.PostEvent("ElevatorTravel", elevatorButton);
+
+        // Room Ambience
+        switch (SceneDestination)
+        {
+            case "LevelScene-2020s":
+                AkSoundEngine.PostEvent("Roomtone2020s", playerObject);
+                break;
+
+            case "LevelScene-1990s":
+                AkSoundEngine.PostEvent("Roomtone1990s", playerObject);
+                break;
+
+            case "LevelScene-1970s":
+                AkSoundEngine.PostEvent("Roomtone1970s", playerObject);
+                break;
+
+            case "LevelScene-1940s":
+                AkSoundEngine.PostEvent("Roomtone1940s", playerObject);
+                break;
+
+            case "LevelScene-30xx":
+                AkSoundEngine.PostEvent("Roomtone30XX", playerObject);
+                break;
+        }
     }
 }
