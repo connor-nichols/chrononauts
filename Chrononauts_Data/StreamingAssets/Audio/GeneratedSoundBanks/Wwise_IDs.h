7797708f72a8063cf94602e99eeb87a2bc98cc5d
Benjamin Cook <90069170+benjaminkcook@users.noreply.github.com>
2023-05-16 15:13:58 -0500

    Ran Unity Build

---
+++ Chrononauts_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,54 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID ELEVATORBUTTONCLICK = 702061953U;
+        static const AkUniqueID ELEVATORDOORSLIDE = 748051766U;
+        static const AkUniqueID ELEVATORTRAVEL = 575746267U;
+        static const AkUniqueID INVENTORYINPUT = 3357836269U;
+        static const AkUniqueID INVENTORYOUTPUT = 2083830872U;
+        static const AkUniqueID ITEMDROPPED = 2601892650U;
+        static const AkUniqueID PLAY_FLORESCENTBUZZ = 3191176734U;
+        static const AkUniqueID PLAY_LAZYDAYBLUES_WAV = 119164842U;
+        static const AkUniqueID RIFT_ITEMPLACED = 3321777685U;
+        static const AkUniqueID RIFT_SOLVED = 752450508U;
+        static const AkUniqueID ROOMTONE30XX = 1552205833U;
+        static const AkUniqueID ROOMTONE1940S = 4240109521U;
+        static const AkUniqueID ROOMTONE1970S = 4139296746U;
+        static const AkUniqueID ROOMTONE1990S = 182147364U;
+        static const AkUniqueID ROOMTONE2020S = 4068587995U;
+        static const AkUniqueID SAFEBOX_DIALCLICK = 1634299854U;
+        static const AkUniqueID SAFEBOX_DOOROPEN = 1545920960U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
